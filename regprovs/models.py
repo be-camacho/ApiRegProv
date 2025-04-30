e4439dc25f135110fdf20a0340708dca073d0437
be-camacho <be.camacho@duocuc.cl>
2025-04-30 16:15:21 -0400

    modelos creados

--- regprovs/models.py
+++ regprovs/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    id_proveedor = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    rut = models.IntegerField()
+    dv = models.CharField(max_length=1)
+    nombre_empresa = models.CharField(max_length=100)
+    rut_empresa = models.IntegerField()
+    puntuacion = models.FloatField()
+
+class Prod_Proveedor(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    nombre_producto = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.FloatField()
+    id_proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    id_marca = models.ForeignKey('Marca', on_delete=models.CASCADE)
+    id_tipo = models.ForeignKey('Tipo', on_delete=models.CASCADE)
+
+class Marca (models.Model):
+    id_marca = models.AutoField(primary_key=True)
+    nombre_marca = models.CharField(max_length=100)
+
+class Tipo(models.Model):
+    id_tipo = models.AutoField(primary_key=True)
+    nombre_tipo = models.CharField(max_length=100)
