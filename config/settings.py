6798a820bd04b64f97aaee2cad1b6808a9bd8e67
be-camacho <be.camacho@duocuc.cl>
2025-05-18 01:31:12 -0400

    .

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-2!&04ly-fdv1yk7=l8yo-2vag$i_q(3$(6owq=yh5nrfg77l)%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.194.212.252',]
 
 
 # Application definition
